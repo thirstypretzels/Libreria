1e725c8df81da23bddc52ab06fb958454adc221c
Kevin Losada <kevin.losada21@gmail.com>
2020-03-15 22:32:24 -0400

    Attempt to fix merge conflict

--- src/components/comments.js
+++ src/components/comments.js
@@ -1,62 +1,58 @@
-import React, { Component } from 'react';
-import { Comment, CommentGroup, Button, Form, Header} from 'semantic-ui-react';
+import React, { Component } from "react";
+import { Comment, CommentGroup, Button, Form, Header } from "semantic-ui-react";
 
 class Comments extends Component {
+  render() {
+    return (
+      <div class='image'>
+        <img
+          src='https://www.mswordcoverpages.com/wp-content/uploads/2018/10/Book-cover-page-7-CRC.png'
+          alt='bookcover'
+          className='book-img'
+        />
 
-    render() {
-        return(
-            
-            <div class="image">
-                <img src="https://www.mswordcoverpages.com/wp-content/uploads/2018/10/Book-cover-page-7-CRC.png"
-                alt="bookcover"
-                className="book-img"
-                />
-               
-               <h2><span>Book Details GO HERE</span></h2>
-               <h1>
-                    StarRating
-            </h1>
-            <Header as='h3' dividing>
-                Comments
-            </Header>
-            
-            <CommentGroup>
-            <Comment>
-                <Comment.Avatar src = "https://www.asiatripdeals.com/wp-content/uploads/2019/03/Anonymous-Avatar.png" />
-                <Comment.Content>
-                    <Comment.Author>
-                        Anonymous
-                    </Comment.Author>
-                    <Comment.Metadata>an hour ago</Comment.Metadata>
-                    <Comment.Text>This book is amazing!</Comment.Text>
-                    <Comment.Action>Reply</Comment.Action>
-                </Comment.Content>
+        <h2>
+          <span>Book Details GO HERE</span>
+        </h2>
+        <h1>StarRating</h1>
+        <Header as='h3' dividing>
+          Comments
+        </Header>
 
-            </Comment>
+        <CommentGroup>
+          <Comment>
+            <Comment.Avatar src='https://www.asiatripdeals.com/wp-content/uploads/2019/03/Anonymous-Avatar.png' />
+            <Comment.Content>
+              <Comment.Author>Anonymous</Comment.Author>
+              <Comment.Metadata>an hour ago</Comment.Metadata>
+              <Comment.Text>This book is amazing!</Comment.Text>
+              <Comment.Action>Reply</Comment.Action>
+            </Comment.Content>
+          </Comment>
 
-            <Comment>
-                <Comment.Avatar src = "https://www.asiatripdeals.com/wp-content/uploads/2019/03/Anonymous-Avatar.png" />
-                <Comment.Content>
-                    <Comment.Author>
-                        Anonymous
-                    </Comment.Author>
-                    <Comment.Metadata>2 days ago</Comment.Metadata>
-                    <Comment.Text>This book is spectacular!</Comment.Text>
-                    <Comment.Action>Reply</Comment.Action>
-                </Comment.Content>
+          <Comment>
+            <Comment.Avatar src='https://www.asiatripdeals.com/wp-content/uploads/2019/03/Anonymous-Avatar.png' />
+            <Comment.Content>
+              <Comment.Author>Anonymous</Comment.Author>
+              <Comment.Metadata>2 days ago</Comment.Metadata>
+              <Comment.Text>This book is spectacular!</Comment.Text>
+              <Comment.Action>Reply</Comment.Action>
+            </Comment.Content>
+          </Comment>
 
-            </Comment>
-
-            <Form reply>
-                <Form.TextArea />
-                <Button content='Add Comment' labelPostion='left' icon='edit' primary/>
-            </Form>
-            </CommentGroup>
-         
-
-            </div>
-        )
-    }
+          <Form reply>
+            <Form.TextArea />
+            <Button
+              content='Add Comment'
+              labelPostion='left'
+              icon='edit'
+              primary
+            />
+          </Form>
+        </CommentGroup>
+      </div>
+    );
+  }
 }
 
 export default Comments;
