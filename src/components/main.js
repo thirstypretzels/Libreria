1dc06e5cc1396e0080359cde6e8c2bc740001a32
Kevin Losada <kevin.losada21@gmail.com>
2020-03-16 21:16:54 -0400

    Added styling to book details page

--- src/components/main.js
+++ src/components/main.js
@@ -1,22 +1,21 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
-import LandingPage from './landingpage';
-import Browse from './browse';
-import Profile from './profile';
-import Wish from './wishlist';
-import Cart from './shoppingcart';
-import Comments from './comments';
-
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import LandingPage from "./landingpage";
+import Browse from "./browse";
+import Profile from "./profile";
+import Wish from "./wishlist";
+import Cart from "./shoppingcart";
+import detailsComments from "./detailsComments";
 
 const Main = () => (
-    <Switch>
-        <Route exact path="/" component={LandingPage} />
-        <Route path="/Browse" component={Browse} />
-        <Route path="/Profile" component={Profile} />
-        <Route path="/Wish" component={Wish} />
-        <Route path="/Cart" component={Cart} />
-        <Route path="/Comments" component={Comments} />
-    </Switch>
-)
+  <Switch>
+    <Route exact path='/' component={LandingPage} />
+    <Route path='/Browse' component={Browse} />
+    <Route path='/Profile' component={Profile} />
+    <Route path='/Wish' component={Wish} />
+    <Route path='/Cart' component={Cart} />
+    <Route path='/Comments' component={detailsComments} />
+  </Switch>
+);
 
 export default Main;
