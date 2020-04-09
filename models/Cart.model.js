410a262388dde2209d8127c2796c4fd9fa025193
thirstypretzels <49509711+thirstypretzels@users.noreply.github.com>
2020-04-09 17:45:44 -0400

    Add files via upload

--- models/Cart.model.js
+++ models/Cart.model.js
@@ -4,9 +4,14 @@ const Schema = mongoose.Schema;
 
 const CartSchema = new mongoose.Schema({
     user: { type: mongoose.Schema.Types.ObjectId, ref: 'User'},
-    books: { type: Array, required: true},
-    price: {type: Number, required: true},
-    quantity: {type: Number, required: true}
+    product: { "type": "array",
+      "maxItems": 2, 
+      "items":[
+                {"type":  mongoose.Schema.Types.ObjectId},
+                {"type": Number}
+              ], 
+        required: true},
+    subtotal: {type: Number, required: true}
   });
   const Carts  = mongoose.model("Carts", CartSchema);
 
