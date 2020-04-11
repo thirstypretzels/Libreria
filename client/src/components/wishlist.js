d897c7c2cf1cff2a72ff64e1071680bfd8da9e3b
Jessman56 <47045407+Jessman56@users.noreply.github.com>
2020-04-11 16:52:11 -0400

    Update wishlist.js

--- client/src/components/wishlist.js
+++ client/src/components/wishlist.js
@@ -1,112 +1,33 @@
-const router = require("express").Router();
-let WishList = require("../../models/WishList.model");
-let Carts = require("../../modelsCart.model");
-let Books = require("../../models/Book.model");
+import React, { Component } from "react";
 
-function searchArray(array, id) {
-  for (let i = 0; i < array.length; i++) {
-    if (array[i][0] == id) {
-      return true;
-    }
+class Wish extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Wish List</h1>
+
+        <p>
+          <form>
+            <input placeholder="Create New List. (Max is 3)" />
+            <button>Create New List</button>
+          </form>
+        </p>
+        <div>
+          <select>
+            <option>Main List</option>
+            <option>Personal List</option>
+          </select>
+          <button>Submit</button>
+        </div>
+
+        <p>
+          <input placeholder="Change List Name" />
+          <button>Rename</button>
+          <button>Delete</button>
+        </p>
+      </div>
+    );
   }
-  return false;
-}
-function findPosition(array, id) {
-  for (let i = 0; i < array.length; i++) {
-    if (array[i][0] == id) {
-      return i;
-    }
-  }
 }
 
-outer.route("/").get((req, res) => {
-  WishList.find()
-    .then((wishList) => res.json(wishList))
-    .catch((err) => res.status(400).json("Error: " + err));
-});
-
-router.route("/add").post((req, res) => {
-  const user = req.body.user;
-  const product = req.body.product;
-  const newWishList = new WishList({ user, product });
-
-  newWishList
-    .save()
-    .then(() => res.json("Wish List added!"))
-    .catch((err) => res.status(400).json("Error: " + err));
-});
-
-router.route("/:id").get((req, res) => {
-  WishList.findById(req.params.id)
-    .then((wishList) => res.json(wishList))
-    .catch((err) => res.status(400).json("Error: " + err));
-});
-
-router.route("/:id").delete((req, res) => {
-  WishList.findByIdAndDelete(req.params.id)
-    .then((wishList) => res.json("Deleted."))
-    .catch((err) => res.status(400).json("Error: " + err));
-});
-
-router.route("/update/:id/:bookId").post((req, res) => {
-  WishList.findById(req.params.id)
-    .then((wishList) => {
-      if (searchArray(wishList.product, req.params.bookId)) {
-        let pos = findPosition(wishList.product, req.params.bookId);
-        Books.findById(req.params.bookId).then((book) => {
-          newBook = book.price;
-          let fin = wishList.product[pos][1] + 1;
-          wishList.product[pos][1] = fin;
-          wishList
-            .save()
-            .then(() => res.json("Wish List Updated!" + pos))
-            .catch((err) => res.status(400).json("Error: " + err));
-        });
-      } else if (!searchArray(carts.product, req.params.bookId)) {
-        wishList.product.push([req.params.bookId, 1]);
-        Books.findById(req.params.bookId).then((book) => {
-          newBook = book.price;
-          wishList
-            .save()
-            .then(() => res.json("Wish List Updated!"))
-            .catch((err) => res.status(400).json("Error: " + err));
-        });
-      } else {
-        let none = null;
-      }
-    })
-    .catch((err) => res.status(400).json("Error: " + err));
-});
-router.route("/updateDelete/:id/:bookId").post((req, res) => {
-  WishList.findById(req.params.id)
-    .then((wishList) => {
-      if (searchArray(wishList.product, req.params.bookId)) {
-        let pos = findPosition(wishList.product, req.params.bookId);
-        wishList.product.splice(pos, 1);
-        Books.findById(req.params.bookId).then((book) => {
-          newBook = book.price;
-          wishList
-            .save()
-            .then(() => res.json("Wish list Updated!" + pos))
-            .catch((err) => res.status(400).json("Error: " + err));
-        });
-      }
-    })
-    .catch((err) => res.status(400).json("Error: " + err));
-});
-
-router.route("/update/:id").post((req, res) => {
-  WishList.findById(req.params.id)
-    .then((wishList) => {
-      WishList.title = req.body.title;
-      WishList.author = req.body.author;
-      WishList.price = Number(req.body.price);
-      WishList.save()
-        .then(() => res.json("Wish List Updated!"))
-        .catch((err) => res.status(400).json("Error: " + err));
-    })
-    .catch((err) => res.status(400).json("Error: " + err));
-});
-
-module.exports = router;
-
+export default Wish;
