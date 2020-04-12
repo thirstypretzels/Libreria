81218bbb2abb6151fed93e03bc0224a1e9f275e1
Jessman56 <47045407+Jessman56@users.noreply.github.com>
2020-04-11 20:09:35 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
 
-require('dotenv').config();
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -13,28 +13,32 @@ app.use(cors());
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
-mongoose.connect("mongodb+srv://mllanes19:mllanes19@cluster0-tehc9.mongodb.net/test?retryWrites=true&w=majority", { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }
+mongoose.connect(
+  "mongodb+srv://mllanes19:mllanes19@cluster0-tehc9.mongodb.net/test?retryWrites=true&w=majority",
+  { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }
 );
 const connection = mongoose.connection;
-connection.once('open', () => {
+connection.once("open", () => {
   console.log("MongoDB database connection established successfully");
-})
+});
 
-const bookRoutes = require('./routes/api/books');
-const cartRoutes = require('./routes/api/carts');
-const userRoutes = require('./routes/api/users');
-const orderRoutes = require('./routes/api/orders');
+const bookRoutes = require("./routes/api/books");
+const cartRoutes = require("./routes/api/carts");
+const userRoutes = require("./routes/api/users");
+const orderRoutes = require("./routes/api/orders");
+const wishRoutes = require("./routes/api/wishList");
 
-app.use('/books', bookRoutes);
-app.use('/users', userRoutes); 
-app.use('/carts', cartRoutes);
-app.use('/orders', orderRoutes);
+app.use("/books", bookRoutes);
+app.use("/users", userRoutes);
+app.use("/carts", cartRoutes);
+app.use("/orders", orderRoutes);
+app.use("/wishList", wishRoutes);
 app.use((req, res, next) => {
-    res.status(404).json({
-        message: 'Not Found'
-    })
-})
+  res.status(404).json({
+    message: "Not Found",
+  });
+});
 
 app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`);
+  console.log(`Server is running on port: ${port}`);
 });
